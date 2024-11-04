26fab4dc6d29715a1975a7c5ff0de6b1bc86b547
Sughosha Vasista <114648260+hubuser2005@users.noreply.github.com>
2024-11-04 15:46:14 +0530

    Add files via upload

---
+++ orprgm.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a=3;
+    printf("what is skanda?\n0-for boy\n1-for girl!\n");
+    if(a==0||a==1){
+        printf("skanda is straight!");
+    }
+    else{
+        printf("\nSkanda is lgbtq+");
+    }
+    return 0;
+}
