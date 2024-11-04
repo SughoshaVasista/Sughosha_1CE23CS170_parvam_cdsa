c7d8193eeeccc14d8defe22dfd9cfad5e3afaaf2
Sughosha Vasista <114648260+hubuser2005@users.noreply.github.com>
2024-11-04 12:15:47 +0530

    Add files via upload

---
+++ prgm3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+     int a=10;
+    float b=12;
+    char c='w';
+    char d='t';
+    char e='f';
+    printf("%d\n%f\n%c%c%c",a,b,c,d,e);
+    char f[]="Juice pilado!";
+    printf("\n%s",f);
+}
